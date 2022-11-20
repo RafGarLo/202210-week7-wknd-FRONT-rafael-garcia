0a395a87f9176378507c25a3fa791ea9579127c4
Rafael <grafa74@gmail.com>
2022-11-20 22:34:09 +0100

    changes for eslint

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-
 import { MemoryRouter as Router } from "react-router-dom";
 import "@testing-library/jest-dom";
 import App from "./App";
