c0dae107459186c189e582abfc338b75d33a795f
Rafael <grafa74@gmail.com>
2022-11-21 18:33:37 +0100

    added buttons

--- src/features/components/robot.item/robot.item.tsx
+++ src/features/components/robot.item/robot.item.tsx
@@ -1,6 +1,8 @@
+import { useRobots } from "../../../infrastructure/hooks/use.robots";
 import { Robot } from "../../../models/robot";
 
 export function RobotItem({ item }: { item: Robot }) {
+    const { handleDelete } = useRobots();
     return (
         <>
             <p>{item.name}</p>
@@ -8,6 +10,13 @@ export function RobotItem({ item }: { item: Robot }) {
             <img src={item.img} alt={"Image of" + item.name} height="200" />
             <p>Endurance: {item.endurance}</p>
             <p>Date of Creation: {item.dateOfCreation}</p>
+            <button
+                onClick={() => {
+                    handleDelete(item);
+                }}
+            >
+                Delete
+            </button>
         </>
     );
 }
--- src/features/components/robot.list/robot.list.tsx
+++ src/features/components/robot.list/robot.list.tsx
@@ -1,14 +1,23 @@
 import { RobotItem } from "../robot.item/robot.item";
 
 import { Robot } from "../../../models/robot";
+import { useRobots } from "../../../infrastructure/hooks/use.robots";
 
 export function RobotList({ item }: { item: Robot[] }) {
+    const { handleAdd } = useRobots();
     return (
         <div>
             <ul>
                 {item.map((item: Robot) => (
                     <li key={item.id}>
                         <RobotItem item={item}></RobotItem>
+                        <button
+                            onClick={() => {
+                                handleAdd(item);
+                            }}
+                        >
+                            Add Robot
+                        </button>
                     </li>
                 ))}
             </ul>
